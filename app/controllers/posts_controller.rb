68502792bdaff83aea98275955e6e53f23e4b0c4
dankalmar <dankalmar@gmail.com>
2013-03-04 20:55:35 -0500

    Root page + postgres

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def new
+		@posts = Post.new
+	end
+
+	def create
+		@post = Post.new(params[:post])
+		if @post.save
+			redirect_to posts_path
+		else
+			render :new
+		end
+
+	end
+
+
 end
